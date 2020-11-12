c87b7edbcb54e049008338793549c7565ada945e
jllopiz90 <jesusllopiz90@gmail.com>
2020-11-12 09:18:59 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import sonImg from './assets/son.png';
 import './App.css';
 import CircularImg from './components/CircularImg';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <CircularImg src={sonImg} alt="son" />      
-    </div>
+    <>
+      <div className="App">
+        <CircularImg src={sonImg} alt="son" />
+      </div>
+      <Footer />
+    </>
   );
 }
 
